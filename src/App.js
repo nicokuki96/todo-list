0ec63f3c33033ba0fdfa040db4012a4062eada9b
nicokuki96 <72584691+nicokuki96@users.noreply.github.com>
2021-06-15 18:16:42 -0300

    cambio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hola <code>src/App.js</code> and save to reload.
+          Gonza PT
         </p>
         <a
           className="App-link"
