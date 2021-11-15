ef87c8aa09c041407e6d8d2ca89f474da7530862
nicokuki96 <72584691+nicokuki96@users.noreply.github.com>
2021-11-14 22:47:31 -0300

    cambios finales

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import PopupAdd from './PopupAdd';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import Fab from '@material-ui/core/Fab';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import Avatar from '@material-ui/core/Avatar';
-import MenuIcon from '@material-ui/icons/Menu';
-import AddIcon from '@material-ui/icons/Add';
-import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
-
+import React, { useState } from "react";
+import PopupAdd from "./PopupAdd";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Fab from "@material-ui/core/Fab";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import Avatar from "@material-ui/core/Avatar";
+import MenuIcon from "@material-ui/icons/Menu";
+import AddIcon from "@material-ui/icons/Add";
+import SearchIcon from "@material-ui/icons/Search";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+import uniqid from "uniqid";
 
 const useStyles = makeStyles((theme) => ({
   text: {
@@ -37,47 +37,91 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
   },
   appBar: {
-    top: 'auto',
+    top: "auto",
     bottom: 0,
   },
   grow: {
     flexGrow: 1,
   },
   fabButton: {
-    position: 'absolute',
+    position: "absolute",
     zIndex: 1,
     top: -30,
     left: 0,
     right: 0,
-    margin: '0 auto',
+    margin: "0 auto",
+  },
+  deleteButton: {
+    marginRight: "10px",
   },
 }));
 
 function BottomAppBar(props) {
+  const [nombre, setNombre] = React.useState("");
+  const [precio, setPrecio] = React.useState("");
+  const [error, setError] = React.useState(null);
   const [open, setOpen] = React.useState(false);
-  const [lista,setLista] = React.useState([]);
+  const [lista, setLista] = React.useState([]);
+  const [isEdit, setIsedit] = useState(false);
+  const [idSelected, setIdselected] = useState(null);
   const classes = useStyles();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!nombre.trim()) {
+      setError("El nombre esta vacio");
+      return;
+    }
+    const nuevoItem = {
+      id: uniqid(),
+      tituloNombre: nombre,
+      tituloPrecio: precio,
+    };
+    setLista([...lista, nuevoItem]);
+    setNombre("");
+    setPrecio("");
+    setError(null);
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
+    setIsedit(false);
+    setNombre("");
+    setPrecio("");
   };
 
   const handleClose = () => {
     setOpen(false);
   };
 
-  const onClickAdd =(item)=>{
-    setLista([...lista, item])
-  }
+  const onClickEdit = (item) => {
+    const { id, tituloNombre, tituloPrecio } = item;
+    setOpen(true);
+    setIsedit(true);
+    setIdselected(id);
+    setNombre(tituloNombre);
+    setPrecio(tituloPrecio);
+  };
 
-  const onClickEdicion = (id, nombre)=>{
-    
-  }
+  const endEdit = () => {
+    const nuevaLista = lista.map((item) => {
+      if (item.id == idSelected)
+        return {
+          id: item.id,
+          tituloNombre: nombre,
+          tituloPrecio: precio,
+        };
+      else {
+        return item;
+      }
+    });
+    setLista(nuevaLista);
+  };
 
   const borrar = (id) => {
-    const nuevoArray = lista.filter (item => item.id !== id) //que concha hace aca
-    setLista(nuevoArray)
-  }
+    const nuevoArray = lista.filter((item) => item.id !== id);
+    setLista(nuevoArray);
+  };
 
   return (
     <React.Fragment>
@@ -86,18 +130,52 @@ function BottomAppBar(props) {
         <Typography className={classes.text} variant="h5" gutterBottom>
           Inbox
         </Typography>
-        <PopupAdd isOpen={open} onClickClose={handleClose} onClickIngresar={onClickAdd}/>
+        <PopupAdd
+          isOpen={open}
+          onClickClose={handleClose}
+          onSubmit={onSubmit}
+          isEdit={isEdit}
+          idSelected={idSelected}
+          nombre={nombre}
+          precio={precio}
+          setNombre={setNombre}
+          setPrecio={setPrecio}
+          lista={lista}
+          error={error}
+          endEdit={endEdit}
+        />
         <List className={classes.list}>
-          {lista.map(item => (
+          {lista.map((item) => (
             <React.Fragment key={item.id}>
-              {item.id === 1 && <ListSubheader className={classes.subheader}>Hoy</ListSubheader>}
+              {item.id === 1 && (
+                <ListSubheader className={classes.subheader}>Hoy</ListSubheader>
+              )}
               <ListItem button>
                 <ListItemAvatar>
                   <Avatar alt="Profile Picture" />
                 </ListItemAvatar>
-                <ListItemText primary={item.tituloNombre} secondary={item.tituloPrecio}/>
-                <Button onClick={() => {borrar(item.id)}} variant="contained" color="secondary"><DeleteIcon /></Button>
-                <Button onClick={() => {onClickEdicion(item.id)}} variant="contained"><EditIcon /></Button>
+                <ListItemText
+                  primary={item.tituloNombre}
+                  secondary={item.tituloPrecio}
+                />
+                <Button
+                  className={classes.deleteButton}
+                  onClick={() => {
+                    borrar(item.id);
+                  }}
+                  variant="contained"
+                  color="secondary"
+                >
+                  <DeleteIcon />
+                </Button>
+                <Button
+                  onClick={() => {
+                    onClickEdit(item);
+                  }}
+                  variant="contained"
+                >
+                  <EditIcon />
+                </Button>
               </ListItem>
             </React.Fragment>
           ))}
@@ -108,7 +186,12 @@ function BottomAppBar(props) {
           <IconButton edge="start" color="inherit" aria-label="open drawer">
             <MenuIcon />
           </IconButton>
-          <Fab color="secondary" onClick={handleClickOpen} aria-label="add" className={classes.fabButton}>
+          <Fab
+            color="secondary"
+            onClick={handleClickOpen}
+            aria-label="add"
+            className={classes.fabButton}
+          >
             <AddIcon />
           </Fab>
           <div className={classes.grow} />
@@ -124,4 +207,4 @@ function BottomAppBar(props) {
   );
 }
 
-export default BottomAppBar
+export default BottomAppBar;
--- src/components/PopupAdd.jsx
+++ src/components/PopupAdd.jsx
@@ -1,60 +1,48 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Alert from '@material-ui/lab/Alert';
-import uniqid from 'uniqid'
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Alert from "@material-ui/lab/Alert";
 
 function PopupAdd(props) {
-
-  const {onClickIngresar,isOpen,onClickClose} = props
-  
-  const [nombre, setNombre] = React.useState('');
-  const [precio, setPrecio] = React.useState('');
-  const [edicion, setEdicion] = React.useState(false);
-  const [setId, Id] = React.useState('');
-  const [error, setError] = React.useState(null);
-  
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-    if(!nombre.trim()){
-        setError('El nombre esta vacio')
-        return
-    }
-    const nuevoItem={
-      id:uniqid(),
-      tituloNombre:nombre,
-      tituloPrecio: precio
-    }
-    onClickIngresar(nuevoItem)
-    setNombre('')
-    setPrecio('')
-    setError(null)
-    
-  }
-
-  const editar = (item) =>{
-    setEdicion(true)
-    setNombre(item.tituloNombre)
-    setId(item.id)
-  }
-  
+  const {
+    error,
+    setPrecio,
+    isOpen,
+    onClickClose,
+    idSelected,
+    isEdit,
+    setNombre,
+    nombre,
+    precio,
+    onSubmit,
+    endEdit,
+  } = props;
 
   return (
     <div>
-      <Dialog open={isOpen} onClose={onClickClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Agregar producto</DialogTitle>
+      <Dialog
+        open={isOpen}
+        onClose={onClickClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">
+          {isEdit ? "Editar Producto" : "Agregar producto"}
+        </DialogTitle>
         <DialogContent>
           <DialogContentText>
-            Complete el siguiente formulario
+            {isEdit
+              ? "Edite y guarde el form"
+              : "Complete el siguiente formulario"}
           </DialogContentText>
           <TextField
-            onChange={(e)=>{setNombre(e.target.value)}}
+            onChange={(e) => {
+              setNombre(e.target.value);
+            }}
             value={nombre}
             autoFocus
             margin="normal"
@@ -63,22 +51,16 @@ function PopupAdd(props) {
             type="text"
             fullWidth
           />
-          {
-            error != null ?
-            (
-              <Alert severity="error">
-                {error}
-              </Alert>
-            )
-            :
-            (
-              <div></div>
-            )
-          }
+          {error != null ? (
+            <Alert severity="error">{error}</Alert>
+          ) : (
+            <div></div>
+          )}
           <TextField
-            onChange={(e)=>{setPrecio(e.target.value)}}
+            onChange={(e) => {
+              setPrecio(e.target.value);
+            }}
             value={precio}
-            autoFocus
             margin="normal"
             id="precio"
             label="Precio"
@@ -90,7 +72,7 @@ function PopupAdd(props) {
           <Button onClick={onClickClose} color="primary">
             Cancelar
           </Button>
-          <Button onClick={onSubmit} color="primary">
+          <Button onClick={isEdit ? endEdit : onSubmit} color="primary">
             Ingresar
           </Button>
         </DialogActions>
@@ -99,4 +81,4 @@ function PopupAdd(props) {
   );
 }
 
-export default PopupAdd
+export default PopupAdd;
